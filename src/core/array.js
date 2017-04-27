c27a3626e427b29cc362f6379cfce3f43d108a40
alucardlockon <alucardlockon3@gmail.com>
2017-04-27 20:37:31 +0800

    2017-04-27 20:37:33

---
+++ src/core/array.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * 2017-04-27 14:14:51
+ * array core functions
+ */
+function forEach(array, iterate) {
+    Array.isArray(array);
+}
+exports.default = forEach;
---
+++ src/core/array.ts
@@ -0,0 +1,9 @@
+/**
+ * 2017-04-27 14:14:51
+ * array core functions
+ */
+function forEach(array,iterate){
+    Array.isArray(array)
+
+}
+export default forEach
