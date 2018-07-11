031a669b14622e7ca89f4f712c32b60890ff3b00
yuwh <alucardlockon3@gmail.com>
2018-07-11 14:48:00 +0800

    update webpack & package.json

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # hui-func
-hui javascript functions
+> hui javascript functions
+
+> 个人写的jsuntils
 
 see doc folder for more details
+
+# build
+```
+# for linux/mac
+npm run build
+# for win
+npm run bulid-win
+```
+
+# test
+```
+npm test
+```
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@
  * Created by yuwh on 2017/4/30.
  */
 module.exports = {
-    entry: './dist/src/main.js',
+    entry: './src/main.js',
     output: {
-        filename: './dist/hui-func.js',
+        filename: './hui-func.js',
         libraryTarget: 'umd',
         library: '_'
     }
